8e15d3f310e465cc798f08acd5e2bf083eed0a95
Brandon <brandon.dlamini95@gmail.com>
2021-10-14 10:19:37 +0200

    fixed apollo server

--- src/graphql.js
+++ src/graphql.js
@@ -1,4 +1,4 @@
-const { ApolloServer } = require("apollo-server-lambda");
+import { ApolloServer } from "apollo-server-lambda";
 import { schema } from "./schema";
 import { resolvers } from "./resolvers";
 
