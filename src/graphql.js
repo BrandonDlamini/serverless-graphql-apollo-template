d4aa5f0f6e0a605a76f8f16cc5ac92b249ac9ccf
Brandon <brandon.dlamini95@gmail.com>
2021-10-14 11:38:00 +0200

    had to downgrade Apollo server to version 2

--- src/graphql.js
+++ src/graphql.js
@@ -6,15 +6,30 @@ const server = new ApolloServer({
   typeDefs: schema,
   resolvers: resolvers,
   playground: true,
+  formatError: (error) => {
+    return error;
+  },
+  formatResponse: (response) => {
+    return response;
+  },
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
   tracing: true,
   introspection: true,
 });
 
-exports.graphqlHandler = server.createHandler({
-  cors: {
-    origin: "*",
-    credentials: true,
-    methods: ["POST", "GET"],
-    allowedHeaders: ["Content-Type", "Origin", "Accept"],
-  },
-});
+exports.graphqlHandler = (event, context, callback) => {
+  const handler = server.createHandler({
+    cors: {
+      origin: "*",
+      credentials: true,
+      methods: ["POST", "GET"],
+      allowedHeaders: ["Content-Type", "Origin", "Accept"],
+    },
+  });
+  return handler(event, context, callback);
+};
