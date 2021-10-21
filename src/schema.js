c6ef6330322270d0c1c77d13cb3ed14bf5d57280
Brandon <brandon.dlamini95@gmail.com>
2021-10-21 15:16:46 +0200

    added roomate  type in Schema

--- src/schema.js
+++ src/schema.js
@@ -1,8 +1,31 @@
 const schema = `
 
-type Query {
-    hello: String!
+"""
+TYPE
+"""
+
+type Roomate {
+    roomieId: String!
+    name: String!
+    email: String!
+    phoneNumber: String!
+    Description: String!
+    area: String!
 }
+
+
+"""
+QUERY
+"""
+type Query {
+    hello: String!,
+    getAllRoomies,
+    getRoomieByArea
+}
+
+
+
+type Mutation
 
 `;
 
