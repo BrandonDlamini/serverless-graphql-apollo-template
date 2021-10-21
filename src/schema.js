2d293918941521963a0dc5f8773425b846a8b38b
Brandon <brandon.dlamini95@gmail.com>
2021-10-21 15:20:36 +0200

    added comments to Schema file

--- src/schema.js
+++ src/schema.js
@@ -4,28 +4,17 @@ const schema = `
 TYPE
 """
 
-type Roomate {
-    roomieId: String!
-    name: String!
-    email: String!
-    phoneNumber: String!
-    Description: String!
-    area: String!
-}
-
-
 """
 QUERY
 """
+
 type Query {
-    hello: String!,
-    getAllRoomies,
-    getRoomieByArea
+    hello: String!
 }
 
-
-
-type Mutation
+"""
+MUTATIONS
+"""
 
 `;
 
